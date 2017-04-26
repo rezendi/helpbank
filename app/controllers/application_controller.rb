a3501d8b237bf3d0dafbd779becfeafb0ee2713e
Jon Evans <jon@rezendi.com>
2017-04-25 19:34:53 -0700

    notices for user checks

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,24 +3,48 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def check_logged_in
-    redirect_to "/" if current_user.nil?
+    if current_user.nil?
+      flash[:notice] = "Sorry, you must be logged in for that"
+      redirect_to "/"
+    end
   end
 
   def check_super_admin
-    redirect_to "/" unless current_user && current_user.is_admin?
+    unless current_user && current_user.is_admin?
+      flash[:notice] = "Sorry, you must be a super admin for that"
+      redirect_to "/"
+    end
   end
   
   def check_community_admin
-    redirect_to "/" and return if @community.nil?
-    redirect_to community_path(@community) and return if current_user.nil?
-    redirect_to community_path(@community) unless current_user.is_admin_of?(@community)
+    if @community.nil?
+      flash[:notice] = "No community set"
+      redirect_to "/" and return
+    end
+    if current_user.nil?
+      flash[:notice] = "Sorry, you must be logged in and a community admin for that"
+      redirect_to community_path(@community) and return
+    end
+    unless current_user.is_admin_of?(@community)
+      flash[:notice] = "Sorry, you must be a community admin for that"
+      redirect_to community_path(@community)
+    end
   end
 
   def check_community_member
     @community = @project.community if @community.nil? and @project.present?
-    redirect_to "/" and return if @community.nil?
-    redirect_to application_community_path(@community) and return if current_user.nil?
-    redirect_to application_community_path(@community) unless current_user.is_member_of?(@community)
+    if @community.nil?
+      flash[:notice] = "No community set"
+      redirect_to "/" and return
+    end
+    if current_user.nil?
+      flash[:notice] = "Sorry, you must be logged in and a community member for that"
+      redirect_to community_path(@community) and return
+    end
+    unless current_user.is_member_of?(@community)
+      flash[:notice] = "Sorry, you must be a community member for that"
+      redirect_to application_community_path(@community)
+    end
   end
 
   def check_community_access
@@ -31,9 +55,18 @@ class ApplicationController < ActionController::Base
   end
 
   def check_project_admin
-    redirect_to "/" and return if @project.nil? or @project.community.nil?
-    redirect_to community_path(@project.community) and return if @project.nil? || current_user.nil?
-    redirect_to community_path(@project.community) unless current_user.is_admin_of?(@project)
+    if @project.nil? or @project.community.nil?
+      flash[:notice] = "No project set"
+      redirect_to "/" and return
+    end
+    if current_user.nil?
+      flash[:notice] = "Sorry, you must be logged in and a community member for that"
+      redirect_to community_path(@project.community) and return
+    end
+    unless current_user.is_admin_of?(@project)
+      flash[:notice] = "Sorry, you must be a community admin for that"
+      redirect_to community_path(@project.community)
+    end
   end
   
   def after_sign_in_path_for(resource)
